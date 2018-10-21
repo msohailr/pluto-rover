9622adb3a30f364df37bfc89cfe51193f61eed27
Sohail <msohail_r@hotmail.com>
2018-10-21 12:12:05 +0100

    Create foundation projects and one test to move rover from 0, 0, N to 0, 1, N with command F

---
+++ PlutoRover/PlutoRover.Test/RoverTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlutoRover.Test
+{
+    [TestClass]
+    public class RoverTest
+    {
+        [TestMethod]
+        public void RoverNavigation_Test()
+        {
+            // First test with a starting position and movement command
+            var rover = new Rover(0, 0, 'N');
+
+            rover.Navigate('F');
+
+            Assert.AreEqual(0, rover.X);
+            Assert.AreEqual(1, rover.Y);
+            Assert.AreEqual('N', rover.Heading);
+        }
+    }
+}
---
+++ PlutoRover/PlutoRover/Rover.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlutoRover
+{
+    public class Rover
+    {
+        #region C'tor
+
+        public Rover(int x, int y, char heading)
+        {
+            X = x;
+            Y = y;
+            Heading = heading;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Navigate(char command)
+        {
+            if (command == 'F' && Heading == 'N')
+            {
+                Y++;
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int X { get; set; }
+        
+        public int Y { get; set; }
+
+        public char Heading { get; set; }
+
+        #endregion
+    }
+}
